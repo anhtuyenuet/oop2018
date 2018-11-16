9da5c4cefd3670cc8750e6f6fcb66091c01cbaaa
Tuyen Nguyen <nguyenhoangtuyen98@gmail.com>
2018-11-16 14:20:31 +0700

    commit

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,37 @@
 package week10;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+
+    public static List<String> getAllFunctions(String path) throws FileNotFoundException, IOException {
+        List<String> List = new ArrayList<>();
+        String content = "";
+        FileReader fr;
+        BufferedReader br;
+        fr = new FileReader(path);
+        br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            content += line + "\n";
+        }
+        br.close();
+        fr.close();
+        int start = content.indexOf("public static");
+
+        return List;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String path = "src\\week9\\Utils.java";
+        List<String> list = Task1.getAllFunctions(path);
+        for (String i : list) {
+            System.out.println("i");
+        }
+    }
 }
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,33 @@
 package week10;
 
+import java.util.Random;
+
 public class Task2 {
+
+    public static double[] BubbleSort(double a[]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    double temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        double[] random = new double[10];
+        Random numb = new Random();
+        for (int i = 0; i < 10; i++) {
+            random[i] = Math.ceil(numb.nextDouble()*100 * 10) / 10; 
+            System.err.print(random[i] + " ");
+        }
+        random = Task2.BubbleSort(random);
+        System.err.println("_______");
+        for (int i = 0; i < 10; i++) {
+            System.err.print(random[i] + " ");
+        }
+    }
 }
